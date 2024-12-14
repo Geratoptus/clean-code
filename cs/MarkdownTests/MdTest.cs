f0db85dc9c75477ea66f97f1279d67c96e7a33fd
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-14 18:48:43 +0500

    fix(MdTests.cs): пофиксил тест на скорость работы: - Убрал ненужные классы - В сравнении добавил каст к (double), теперь проверка корректная - Добавил GC.Collect() перед замером времени для более корректной проверки

--- cs/MarkdownTests/MdTest.cs
+++ cs/MarkdownTests/MdTest.cs
@@ -1,33 +1,32 @@
 using System.Diagnostics;
 using System.Text;
 using FluentAssertions;
-using Markdown.Generator;
-using Markdown.Parser.Rules;
-using Markdown.Tokenizer;
+using Markdown;
 
 namespace MarkdownTests;
 
 public class Tests
 {
-    private readonly BodyRule rule = new();
-    private readonly HtmlGenerator generator = new();
-    private readonly MdTokenizer tokenizer = new();
-    
     [Test]
     public void Markdown_Render_ShouldWorkFast()
     {
         const int scale = 2;
         var sw = new Stopwatch();
         var results = new List<TimeSpan>();
-        for (var len = 640; len <= 5120; len *= scale)
+        for (var len = 640; len <= 655360; len *= scale)
         {
             var markdown = GenerateMarkdown(len);
-            sw.Start(); RenderMarkdown(markdown); sw.Stop();
-            results.Add(sw.Elapsed); sw.Reset();
+            GC.Collect();
+            sw.Start();
+            Md.Render(markdown);
+            sw.Stop();
+            
+            results.Add(sw.Elapsed);
+            sw.Reset();
         }
         
         Enumerable.Range(1, results.Count - 1)
-            .Select(i => results[i].Ticks / results[i - 1].Ticks)
+            .Select(i => (double)results[i].Ticks / results[i - 1].Ticks)
             .Should().OnlyContain(timeRatio => timeRatio < scale * scale);
     }
     private static string GenerateMarkdown(int len)
@@ -39,11 +38,5 @@ public class Tests
         var allElements = specElements.Concat(alphabet).ToList();
         return Enumerable.Range(0, len).Aggregate(new StringBuilder(), 
             (sb, _) => sb.Append(allElements[rand.Next(allElements.Count)])).ToString();
-    }
-    private void RenderMarkdown(string markdown)
-    {
-        var tokens = tokenizer.Tokenize($"{markdown}\n");
-        var root = rule.Match(tokens);
-        generator.Render(root, tokens);
     }
 }
